89ee042b53852f626f43e83b7223edd2a4d694f4
Christopherhalv0rsen <chrishp1997@hotmail.com>
2023-02-10 13:01:16 +0100

    main stamme
    npm og sass hovedfil

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import './css/main.css'
+
+function App() {
+  return (
+    <body>
+    <main>
+  
+    <h1>hei</h1>
+    
+    </main>
+    </body>
+  );
+}
+
+export default App;
