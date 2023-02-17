5a19b9b0af1b3f28342a249dd1c590e2ecce00fb
Christopherhalv0rsen <chrishp1997@hotmail.com>
2023-02-17 09:31:43 +0100

    css/layout
    Endring av sass pluss midlertidig oppsett av html i app.js

--- src/App.js
+++ src/App.js
@@ -5,11 +5,28 @@ import './css/main.css'
 function App() {
   return (
     <body>
+    <div class="container">
+    <header>
+      <h1>Ressursarkiv</h1>
+    </header>
+    <nav>
+      <button class="toggle">fane</button>
+      <button class="toggle">fane</button>
+      <button class="toggle">fane</button>
+      <button class="toggle">fane</button>
+      <button class="toggle">fane</button>
+    </nav>
     <main>
-  
-    <h1>hei</h1>
+      <article class="product-card"></article>
+        <h3>hei</h3>
+          <p>lorem</p>
+            <ul>
+              <li>test</li>
+            </ul>
+      <article></article>
     
     </main>
+    </div>
     </body>
   );
 }
