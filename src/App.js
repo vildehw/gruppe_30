48beb7db71443d78b6c9de69ea9fba2efefe3e51
vildehw <vildehw@hiof.no>
2023-02-24 13:04:15 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,19 +2,21 @@ import './App.css';
 import './css/main.css'
 import Map from './components/Map';
 import resources from './components/ressurser';
+import Button from './components/Button';
 
 
 function App() {
   return ( 
     <body> 
-      <header>
+      <header> 
+        <h1>Ressursarkiv</h1>
         <nav>
           <Button/>
         </nav>
       </header>
     <main>
 
-      <Map resources={resources}/> 
+      
   
     </main> 
     </body>
