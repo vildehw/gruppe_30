913de61ae6aad178b81a279f8b15525d0daaffbd
kollegard <mathias@kollegard.net>
2023-02-24 12:55:50 +0100

    lagt til filter og map for å få en kategori om gangen

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,3 +1,11 @@
-export default function Map(){
-    return()
+import recources from "./ressurser"
+
+export default function Map(recources){
+    return(
+            <>
+            {resources.filter((page) => (
+                page.category.includes("html"))).map(
+                    (info) => <li>{info.title}</li>)}
+            </>
+    )
 }
