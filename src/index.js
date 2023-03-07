6c1222641d1f31aa870bfe75c1c34157fa52c0ae
Christopherhalv0rsen <chrishp1997@hotmail.com>
2023-03-07 13:24:02 +0100

    .
    .

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { createRoot } from 'react-dom/client';
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+reportWebVitals();
